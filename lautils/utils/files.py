90130f91af8aaa35c012ee9aba50901b649b9ad9
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2023-09-01 21:36:32 +0200

    get_caller_filename -> get_caller_file, return LPath

--- lautils/utils/files.py
+++ lautils/utils/files.py
@@ -1,10 +1,12 @@
 import inspect
 
+from .lpath import LPath
+
 __all__: list[str] = [
-    "get_caller_filename"
+    "get_caller_file",
 ]
 
 
-def get_caller_filename() -> str:
-    """Obtain the caller's filename."""
-    return inspect.stack()[2].filename
+def get_caller_file() -> LPath:
+    """Obtain the caller's file as an LPath."""
+    return LPath(inspect.stack()[2].filename)
